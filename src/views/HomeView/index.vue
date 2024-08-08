3a3540d606fdd797f888fd90d23881273ceaaa16
Max Aqquepucho <maxaqquepucho@gmail.com>
2024-08-08 18:34:29 -0500

    feat add header a footer component

---
+++ src/views/HomeView/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  name: 'Home-View'
+}
+</script>
+<script setup lang="ts">
+//COMPONENTS
+import VHeader from '@/components/VHeader.vue'
+import VFooter from '@/components/VFooter.vue'
+</script>
+<template>
+  <main>
+    <VHeader />
+    <VFooter />
+  </main>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindcssForms from '@tailwindcss/forms'
+
+// const tailwindcssForms = require('@tailwindcss/forms')
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [tailwindcssForms]
 }

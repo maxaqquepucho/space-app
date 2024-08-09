e8cd2506b306f14263786406bb1d9d5452dea161
Max Aqquepucho <maxaqquepucho@gmail.com>
2024-08-08 21:11:05 -0500

    fix: path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'space-app',
+  base: '/space-app',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
